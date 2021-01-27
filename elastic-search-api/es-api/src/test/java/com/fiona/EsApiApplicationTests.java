7e9aa4dbaa02128cae9a399d428da4c535b61330
fiona8231 <9246068@qq.com>
2021-01-27 07:37:44 +0100

    Init Commit

--- elastic-search-api/es-api/src/test/java/com/fiona/EsApiApplicationTests.java
+++ elastic-search-api/es-api/src/test/java/com/fiona/EsApiApplicationTests.java
@@ -2,13 +2,18 @@ package com.fiona;
 
 import com.alibaba.fastjson.JSON;
 import com.fiona.pojo.User;
+import org.apache.lucene.util.QueryBuilder;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
@@ -19,6 +24,10 @@ import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +35,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JsonbTester;
 
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 @SpringBootTest
 class EsApiApplicationTests {
@@ -124,6 +135,56 @@ class EsApiApplicationTests {
 
 		DeleteResponse deleteResponse = client.delete(deleteRequest, RequestOptions.DEFAULT);
 		System.out.println(deleteResponse.status());
+	}
+
+	//#真正的项目-> 批量插入数据
+	@Test
+	void testBulkRequest() throws IOException {
+		BulkRequest bulkRequest = new BulkRequest();
+		//插入时间越长，设置时间越长，这里自己看
+		bulkRequest.timeout("10s");
+
+		ArrayList<User> list = new ArrayList<>();
+		list.add(new User("yue3", 2));
+		list.add(new User("yue1", 3));
+		list.add(new User("yue2", 6));
+		list.add(new User("yue4", 10));
+		list.add(new User("yue5", 12));
+		list.add(new User("yue6", 22));
+		list.add(new User("yue7", 24));
+
+		for (int i = 0; i < list.size() ; i++) {
+			bulkRequest.add(
+			  new IndexRequest("kuangshen").id(" " + i).source(JSON.toJSONString(list.get(i)), XContentType.JSON)
+			);
+		}
+
+		BulkResponse responses = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+		System.out.println(responses.hasFailures());
+    }
+
+    @Test
+    void testSearch() throws IOException {
+		SearchRequest searchRequest = new SearchRequest("kuangshen");
+
+		//构建查询条件
+		SearchSourceBuilder searchBuilder = new SearchSourceBuilder();
+
+		//查询条件，我们可以使用 [QueryBuilders] 工具来实现
+		//term 精确查询，不会分词
+		TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("name", "yue1");
+		searchBuilder.query(termQueryBuilder);
+
+
+		searchRequest.source(searchBuilder);
+		SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+		//obj转换成Json
+		System.out.println(JSON.toJSONString(searchResponse.getHits()));
+		System.out.println("++++++++++++++++++++++");
+		for (SearchHit hit : searchResponse.getHits()) {
+			hit.getSourceAsMap();
+		}
+
 	}
 
 }
